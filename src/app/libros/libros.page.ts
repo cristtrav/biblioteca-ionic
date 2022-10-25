6614958f6175f8f379f5f83fa2de163415f07831
Cristhian Traversi <crist.trav@gmail.com>
2022-10-25 19:19:21 -0300

    Tarea 2 Ionic (Parte 3)

--- src/app/libros/libros.page.ts
+++ src/app/libros/libros.page.ts
@@ -14,6 +14,7 @@ export class LibrosPage implements OnInit {
 
   public listaLibros: Libro[] = [];
   public cargandoLibros: boolean = false;
+  public modalVisible: boolean = false;
 
   constructor(
     private servicioLibros: LibrosService,
@@ -45,6 +46,10 @@ export class LibrosPage implements OnInit {
 
       }
     });
+  }
+
+  public nuevo(){
+    this.modalVisible = true;
   }
 
 }
